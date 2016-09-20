36f1cdc6059360aed52cb14f301ad1c4f11d06c5
Karunesh Mahajan <Karunesh Mahajan>
2016-09-19 21:23:37 -0400

    Initial Commit

---
+++ FlashCards/app/src/main/java/edu/bu/karunesh/ec591/flashcards/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.bu.karunesh.ec591.flashcards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
