4dda7e0265e4b9876d56e421207c9eae4b60fc19
seshank4 <seshank4@gmail.com>
2016-09-20 21:12:43 -0400

    Submit logic

--- FlashCards/app/src/main/java/edu/bu/karunesh/ec591/flashcards/MainActivity.java
+++ FlashCards/app/src/main/java/edu/bu/karunesh/ec591/flashcards/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox cbSubtration;
     private Boolean addition = false;
     private Boolean subtraction = false;
+    private static int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,4 +40,40 @@ public class MainActivity extends AppCompatActivity {
         cbSubtration.setEnabled(false);
         btnOperation.setVisibility(View.INVISIBLE);
     }
+
+    public void doSubmit(View view) {
+
+        TextView operand1 = (TextView) findViewById(R.id.operand1);
+        TextView operand2 = (TextView) findViewById(R.id.operand2);
+        EditText answer = (EditText) findViewById(R.id.answer);
+
+
+        int firstNumber = Integer.parseInt(operand1.getText().toString());
+        int secondNumber = Integer.parseInt((operand2.getText().toString()));
+        int ans = Integer.parseInt(answer.getText().toString());
+
+        TextView operator = (TextView)findViewById(R.id.operator);
+
+        String operatorVal = operator.getText().toString();
+
+        if(operatorVal.equals("+")){
+            if(ans == (firstNumber + secondNumber)){
+                count ++;
+            }
+        }else{
+            if(ans == (firstNumber - secondNumber)){
+                count ++;
+            }
+        }
+
+        operand1.setText("");
+        operand2.setText("");
+        operator.setText("");
+        answer.setText("");
+
+
+    }
+
 }
+
+
